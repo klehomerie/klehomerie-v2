8453484072f9c3a05b5efa6987dc1a340fdbd66d
klehomerie <azerdab@klehomerie.com>
2026-02-22 09:52:53 +0000

    refinement formatting + visual contents

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/assets");
   eleventyConfig.addPassthroughCopy("./src/robots.txt");
   eleventyConfig.addPassthroughCopy("./src/sitemap.xml");
+  eleventyConfig.addPassthroughCopy("src/admin");
   // Watch for changes
   eleventyConfig.addWatchTarget("./src/assets/css/");
   eleventyConfig.addWatchTarget("./src/assets/js/");
