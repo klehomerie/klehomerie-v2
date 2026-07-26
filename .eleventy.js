aa62545b37056e43690b7e04254404fb05453c14
klehomerie <azerdab@klehomerie.com>
2026-07-26 07:26:19 +0000

    eleventy valid

--- .eleventy.js
+++ .eleventy.js
@@ -68,7 +68,6 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/assets/images");
   eleventyConfig.addPassthroughCopy("src/assets/js");
   eleventyConfig.addPassthroughCopy("src/admin");
-  eleventyConfig.addPassthroughCopy("src/robots.txt");
   eleventyConfig.addPassthroughCopy("src/*.pdf");
   eleventyConfig.addPassthroughCopy({
     "node_modules/vanilla-cookieconsent/dist/cookieconsent.css": "assets/css/cookieconsent.css",
